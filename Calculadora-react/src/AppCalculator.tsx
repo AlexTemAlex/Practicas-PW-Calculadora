ab4f13c34a73f93228028f67871348dbbc00fa28
AlexisCh <temporalxav@outlook.es>
2026-04-21 13:02:20 -0500

    feat: add button list

--- Calculadora-react/src/AppCalculator.tsx
+++ Calculadora-react/src/AppCalculator.tsx
@@ -5,10 +5,43 @@ type AppCalculadorProps = {
 };
 
 function AppCalculador({ functionBackMenu }: AppCalculadorProps) {
+  const buttons = [
+    { label: "7", type: "number" },
+    { label: "8", type: "number" },
+    { label: "9", type: "number" },
+    { label: "/", type: "operator" },
+
+    { label: "4", type: "number" },
+    { label: "5", type: "number" },
+    { label: "6", type: "number" },
+    { label: "*", type: "operator" },
+
+    { label: "1", type: "number" },
+    { label: "2", type: "number" },
+    { label: "3", type: "number" },
+    { label: "-", type: "operator" },
+
+    { label: "0", type: "number" },
+    { label: ".", type: "decimal" },
+    { label: "=", type: "equal" },
+    { label: "+", type: "operator" },
+
+    { label: "C", type: "clear" },
+    { label: "⌫", type: "delete" },
+  ];
+
+  const listButtons = buttons.map((element, idx) => (
+    <Button key={idx} text={element.label}></Button>
+  ));
+
   return (
     <>
       <Button text="Back" onClick={() => functionBackMenu()}></Button>
-      <p>Hola</p>
+      <br />
+      <br />
+
+      <input type="text" value={0} readOnly />
+      <div>{listButtons}</div>
     </>
   );
 }
--- Calculadora-react/src/Button.tsx
+++ Calculadora-react/src/Button.tsx
@@ -1,12 +1,12 @@
 type MenuProps = {
   text: string;
-  onClick: () => void;
+  onClick?: () => void;
 };
 
 function Button({ text, onClick }: MenuProps) {
   return (
     <>
-      <button type="button" onClick={() => onClick()}>
+      <button type="button" onClick={() => onClick?.()}>
         {text}
       </button>
     </>
