71602aebce0118c67314fc7aee5124b23935e31a
AlexisCh <temporalxav@outlook.es>
2026-04-22 16:31:50 -0500

    feat: add the Shunting Yard algorithm to solve mathematical operations

--- Calculadora-react/src/AppCalculator.tsx
+++ Calculadora-react/src/AppCalculator.tsx
@@ -34,6 +34,13 @@ function AppCalculador({ functionBackMenu }: AppCalculadorProps) {
 
   const [operation, setOperation] = useState("");
 
+  const precedence: Record<string, number> = {
+    "+": 1,
+    "-": 1,
+    "*": 2,
+    "/": 2,
+  };
+
   const getHandler = (type: string, label: string) => {
     switch (type) {
       case "number":
@@ -60,7 +67,87 @@ function AppCalculador({ functionBackMenu }: AppCalculadorProps) {
   });
 
   const tokenize = (textInput: string) => {
-    return textInput.match(/\d+\.?\d*|\(|\)|[+\-*/]/g) || null;
+    return textInput.match(/\d+\.?\d*|[+\-*/()]/g) || [];
+  };
+
+  const normalize = (expr: string) => {
+    return expr.replace(/\(-/g, "(0-");
+  };
+
+  /*
+  Shunting Yard's algorithm, created by Edsger Dijkstra, 
+  converts mathematical expressions from infix notation (normal writing) to
+  postfix notation (RPN) (stack).
+  */
+  const toPostfix = (tokens: string[]): string[] => {
+    const output: string[] = [];
+    const stack: string[] = [];
+
+    tokens.forEach((token) => {
+      if (!isNaN(Number(token))) {
+        output.push(token);
+      } else if (token === "(") {
+        stack.push(token);
+      } else if (token === ")") {
+        while (stack.length && stack[stack.length - 1] !== "(") {
+          output.push(stack.pop()!);
+        }
+        stack.pop(); // quitar "("
+      } else {
+        while (
+          stack.length &&
+          precedence[stack[stack.length - 1]] >= precedence[token]
+        ) {
+          output.push(stack.pop()!);
+        }
+        stack.push(token);
+      }
+    });
+
+    return [...output, ...stack.reverse()];
+  };
+
+  const evaluatePostfix = (postfix: string[]): number => {
+    const stack: number[] = [];
+
+    postfix.forEach((token) => {
+      if (!isNaN(Number(token))) {
+        stack.push(Number(token));
+      } else {
+        const b = stack.pop()!;
+        const a = stack.pop()!;
+
+        switch (token) {
+          case "+":
+            stack.push(a + b);
+            break;
+          case "-":
+            stack.push(a - b);
+            break;
+          case "*":
+            stack.push(a * b);
+            break;
+          case "/":
+            stack.push(a / b);
+            break;
+        }
+      }
+    });
+
+    return stack[0];
+  };
+
+  const validateParentheses = (expr: string): boolean => {
+    let balance = 0;
+
+    for (let char of expr) {
+      if (char === "(") balance++;
+      if (char === ")") balance--;
+
+      if (balance < 0) return false;
+    }
+
+    return balance === 0;
   };
 
   const isOperator = (char: string) => ["*", "/", "+", "-"].includes(char);
@@ -69,6 +156,7 @@ function AppCalculador({ functionBackMenu }: AppCalculadorProps) {
     setOperation((prev) => {
       const last = prev.slice(-1);
       const secondLast = prev.slice(-2, -1);
+
       // restrictions at the beginning
       if (!prev) {
         if (isOperator(value) && value !== "-") return prev;
@@ -113,7 +201,20 @@ function AppCalculador({ functionBackMenu }: AppCalculadorProps) {
   };
 
   const handlerEqual = () => {
-    alert(tokenize(operation));
+    try {
+      if (!validateParentheses(operation)) {
+        alert("Error-Some of the parentheses are not closed");
+        return;
+      }
+      const normalized = normalize(operation);
+      const tokens = tokenize(normalized);
+      const postFix = toPostfix(tokens);
+      const result = evaluatePostfix(postFix);
+
+      setOperation(result.toString());
+    } catch (error) {
+      alert("Error " + error);
+    }
   };
 
   return (
